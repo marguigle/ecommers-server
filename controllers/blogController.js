1dc12e7838baacaac9f46491c0b813bd3bd7ae9d
Marcelo G Iglesia <marguigle@yahoo.com.ar>
2024-08-14 23:44:40 -0300

    se crea controladores de blog

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -5,12 +5,60 @@ import validateMongoDbId from "../utils/validateMongoDbID.js";
 
 export const createBlog = asyncHandler(async (req, res) => {
   try {
-    console.log("Controller triggered");
     const newBlog = await Blog.create(req.body);
-    console.log(newBlog);
+
+    res.status(200).json(newBlog);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+export const updateBlog = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  try {
+    const blogUpdated = await Blog.findByIdAndUpdate(id, req.body, {
+      new: true,
+    });
+
+    res.status(200).json(blogUpdated);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+export const getBlog = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  try {
+    const getBlog = await Blog.findById(id);
+    await Blog.findByIdAndUpdate(
+      id,
+      {
+        $inc: { numViews: 1 },
+      },
+      { new: true }
+    );
+
+    res.status(200).json(getBlog);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+export const getBlogs = asyncHandler(async (req, res) => {
+  try {
+    const allBlogs = await Blog.find();
+
+    res.status(200).json(allBlogs);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+export const deleteBlog = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  try {
+    const deletedBlog = await Blog.findByIdAndDelete(id);
+
     res.json({
-      status: "success",
-      newBlog,
+      deletedBlog: deletedBlog,
+      nessage: "The blog was deleted successfully",
     });
   } catch (error) {
     throw new Error(error);
--- routes/blogRoute.js
+++ routes/blogRoute.js
@@ -1,8 +1,18 @@
 import express from "express";
-import { createBlog } from "../controllers/blogController.js";
+import {
+  createBlog,
+  updateBlog,
+  getBlogs,
+  getBlog,
+  deleteBlog,
+} from "../controllers/blogController.js";
 import { authMiddleware, isAdmin } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 router.post("/", authMiddleware, isAdmin, createBlog);
+router.put("/:id", authMiddleware, isAdmin, updateBlog);
+router.get("/:id", authMiddleware, isAdmin, getBlog);
+router.delete("/:id", authMiddleware, isAdmin, deleteBlog);
+router.get("/", getBlogs);
 
 export default router;
